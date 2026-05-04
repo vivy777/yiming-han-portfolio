cce13f176a5269099cbce248d2874e3688bd6d4c
Yiming Han <hanyiming5@gmail.com>
2026-05-03 23:35:51 -0500

    add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Navbar />
         <main className="max-w-2xl mx-auto px-4 py-16">{children}</main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
